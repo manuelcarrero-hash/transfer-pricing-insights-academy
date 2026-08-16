67610ef04b3a2a06e5d1e0840357dc0bb11e6d5c
manuelcarrero-hash <manuelcarrero@gmail.com>
2026-08-15 22:43:34 -0600

    Route Junior Foundations certificate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,11 +11,12 @@ import { J4CoursePage } from '../pages/J4CoursePage';
 import { J4LessonPage } from '../pages/J4LessonPage';
 import { J5CoursePage } from '../pages/J5CoursePage';
 import { J5LessonPage } from '../pages/J5LessonPage';
+import { JuniorCertificatePage } from '../pages/JuniorCertificatePage';
 import { LessonPage } from '../pages/LessonPage';
 import { MyPathPage } from '../pages/MyPathPage';
 import { StartPage } from '../pages/StartPage';
 import { StudyGuidePage } from '../pages/StudyGuidePage';
 
 export function App() {
-  return <AppShell><ScrollToTop /><Routes><Route path="/" element={<HomePage />} /><Route path="/start" element={<StartPage />} /><Route path="/path" element={<MyPathPage />} /><Route path="/courses/j1" element={<CoursePage />} /><Route path="/courses/j1/study-guide" element={<StudyGuidePage />} /><Route path="/courses/j1/lesson/:lessonNumber" element={<LessonPage />} /><Route path="/courses/j1/lesson-:lessonNumber" element={<Navigate to="/courses/j1" replace />} /><Route path="/courses/j2" element={<J2CoursePage />} /><Route path="/courses/j2/lesson/:lessonNumber" element={<J2LessonPage />} /><Route path="/courses/j3" element={<J3CoursePage />} /><Route path="/courses/j3/lesson/:lessonNumber" element={<J3LessonPage />} /><Route path="/courses/j4" element={<J4CoursePage />} /><Route path="/courses/j4/lesson/:lessonNumber" element={<J4LessonPage />} /><Route path="/courses/j5" element={<J5CoursePage />} /><Route path="/courses/j5/lesson/:lessonNumber" element={<J5LessonPage />} /><Route path="*" element={<Navigate to="/" replace />} /></Routes></AppShell>;
+  return <AppShell><ScrollToTop /><Routes><Route path="/" element={<HomePage />} /><Route path="/start" element={<StartPage />} /><Route path="/path" element={<MyPathPage />} /><Route path="/junior-foundations/certificate" element={<JuniorCertificatePage />} /><Route path="/courses/j1" element={<CoursePage />} /><Route path="/courses/j1/study-guide" element={<StudyGuidePage />} /><Route path="/courses/j1/lesson/:lessonNumber" element={<LessonPage />} /><Route path="/courses/j1/lesson-:lessonNumber" element={<Navigate to="/courses/j1" replace />} /><Route path="/courses/j2" element={<J2CoursePage />} /><Route path="/courses/j2/lesson/:lessonNumber" element={<J2LessonPage />} /><Route path="/courses/j3" element={<J3CoursePage />} /><Route path="/courses/j3/lesson/:lessonNumber" element={<J3LessonPage />} /><Route path="/courses/j4" element={<J4CoursePage />} /><Route path="/courses/j4/lesson/:lessonNumber" element={<J4LessonPage />} /><Route path="/courses/j5" element={<J5CoursePage />} /><Route path="/courses/j5/lesson/:lessonNumber" element={<J5LessonPage />} /><Route path="*" element={<Navigate to="/" replace />} /></Routes></AppShell>;
 }

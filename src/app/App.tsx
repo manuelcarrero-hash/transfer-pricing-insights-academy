6dd90d79177a4caa9b34b3473f829a6bd3476546
manuelcarrero-hash <manuelcarrero@gmail.com>
2026-08-15 20:07:23 -0600

    Add scroll reset and study guide route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppShell } from '../components/layout/AppShell';
+import { ScrollToTop } from '../components/layout/ScrollToTop';
 import { CoursePage } from '../pages/CoursePage';
 import { HomePage } from '../pages/HomePage';
 import { LessonPage } from '../pages/LessonPage';
 import { StartPage } from '../pages/StartPage';
+import { StudyGuidePage } from '../pages/StudyGuidePage';
 
 export function App() {
   return (
     <AppShell>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/start" element={<StartPage />} />
         <Route path="/courses/j1" element={<CoursePage />} />
+        <Route path="/courses/j1/study-guide" element={<StudyGuidePage />} />
         <Route path="/courses/j1/lesson/:lessonNumber" element={<LessonPage />} />
         <Route path="/courses/j1/lesson-:lessonNumber" element={<Navigate to="/courses/j1" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />

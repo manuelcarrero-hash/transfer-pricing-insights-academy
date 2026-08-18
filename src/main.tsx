f7ee48dbb4b4ba291006304f3301f886f7a81d38
manuelcarrero-hash <manuelcarrero@gmail.com>
2026-08-18 13:11:58 -0600

    style: load P0-F assessment and certificate polish

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './styles/p0b-home-shell.css';
 import './styles/path-journey.css';
 import './styles/p0d-course-lesson.css';
 import './styles/p0e-resources.css';
+import './styles/p0f-assessment-certificate.css';
 import './styles/accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
